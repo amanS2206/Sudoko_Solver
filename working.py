a5053ec7f761a7f218d9f2f2d0d5363aca93649a
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-10-30 23:03:20 +0530

    compelete sudoko solver with backtracking

--- working.py
+++ working.py
@@ -23,8 +23,6 @@ def display_board(b):
                 print(str(b[i][j]) + " ", end="")
 
 
-display_board(board)
-
 def find_empty_block(b):
     for i in range(len(b)):
         for j in range(len(b[0])):
@@ -38,17 +36,17 @@ def find_empty_block(b):
 def valid(b, num, pos):
     # check row
     for i in range(len(b[0])):
-        if b[pos[0]][i] == num and pos[0] != i:
+        if b[pos[0]][i] == num and pos[1] != i:
             return False
 
     # check columns
     for j in range(len(b)):
-        if b[i][pos[1]] == num and pos[1] != j:
+        if b[j][pos[1]] == num and pos[0] != j:
             return False
 
     # check box
-    box_x = pos[0] // 3
-    box_y = pos[1] // 3
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x*3, box_x*3 + 3):
@@ -62,7 +60,7 @@ def valid(b, num, pos):
 def solve(b):
     find = find_empty_block(b)
     if not find:
-        return False
+        return True
     else:
         row, col = find
 
@@ -76,3 +74,8 @@ def solve(b):
                 b[row][col] = 0
 
         return False
+
+display_board(board)
+solve(board)
+print("**************************************")
+display_board(board)
