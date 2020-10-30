18893d42c6b13a55d9f046d0e9352890e664b13d
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-10-30 22:23:58 +0530

    add valid funtion

--- working.py
+++ working.py
@@ -30,3 +30,28 @@ def find_empty_block(b):
         for j in range(len(b[0])):
             if b[i][j] == 0:
                 return (i,j)
+
+
+
+def value(b, num, pos):
+    # check row
+    for i in range(len(b[0])):
+        if b[pos[0]][i] == num and pos[0] != i:
+            return False
+
+    # check columns
+    for j in range(len(b)):
+        if b[i][pos[1]] == num and pos[1] != j:
+            return False
+
+    # check box
+    box_x = pos[0] // 3
+    box_y = pos[1] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if b[i][j] == num and (i,j) != pos:
+                return False
+
+
+    return True
